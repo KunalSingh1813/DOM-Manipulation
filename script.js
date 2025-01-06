ec7931458d1b6c0ef1a0ac064eb78bc641d96b40
Kunal Singh <singh.kunal1813@gmail.com>
2025-01-06 22:35:40 +0530

    added filter buttons functionality

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ function LoadTodos() {
   return todos;
 }
 
-function addTodoToLocalStorage(todoText) {
+function addTodoToLocalStorage(todo) {
   const todos = LoadTodos();
-  todos.todoList.push(todoText);
+  todos.todoList.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function appendTodoInHTML(todotext) {
+function appendTodoInHTML(todo) {
   const todoList = document.getElementById("todolist");
   const todoItem = document.createElement("li");
+  const textDiv = document.createElement("div");
 
-  todoItem.textContent = todotext;
+  textDiv.textContent = todo.text;
   todoItem.classList.add("todoItem");
+
+  const wrapperDiv = document.createElement("div");
+  wrapperDiv.classList.add("todoButtons");
 
   //creating edit button
   const editBtn = document.createElement("button");
@@ -26,7 +30,7 @@ function appendTodoInHTML(todotext) {
   //creating delete button
   const DeleteBtn = document.createElement("button");
   DeleteBtn.textContent = "Delete";
-  DeleteBtn.classList.add("DeleteBtn");
+  DeleteBtn.classList.add("deleteBtn");
 
   //Creating completed button
   const completedBtn = document.createElement("button");
@@ -34,11 +38,37 @@ function appendTodoInHTML(todotext) {
   completedBtn.classList.add("completedBtn");
 
   //Appending todo and buttons to todoItem
-  todoItem.appendChild(editBtn);
-  todoItem.appendChild(DeleteBtn);
-  todoItem.appendChild(completedBtn);
-
+  wrapperDiv.appendChild(editBtn);
+  wrapperDiv.appendChild(DeleteBtn);
+  wrapperDiv.appendChild(completedBtn);
+  todoItem.appendChild(textDiv);
+  todoItem.appendChild(wrapperDiv);
   todoList.appendChild(todoItem);
+}
+
+function executeFilterAction(event) {
+  const element = event.target;
+  const value = element.getAttribute("data-filter");
+  const todoList = document.getElementById("todolist");
+  const todos = LoadTodos();
+  if (value == "all") {
+    todoList.innerHTML = "";
+    todos.todoList.forEach((todo) => {
+      appendTodoInHTML(todo);
+    });
+  } else if (value == "pending") {
+    todos.todoList.forEach((todo) => {
+      if (todo.isCompleted != true) {
+        appendTodoInHTML(todo);
+      }
+    });
+  } else {
+    todos.todoList.forEach((todo) => {
+      if (todo.isCompleted == true) {
+        appendTodoInHTML(todo);
+      }
+    });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -48,13 +78,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const todoList = document.getElementById("todolist");
 
+  const filterBtns = document.getElementsByClassName("filterBtn");
+
+  ///filter buttons functionality
+  for (const btn of filterBtns) {
+    btn.addEventListener("click", executeFilterAction);
+  }
+
   submitBtn.addEventListener("click", (event) => {
     const todoText = todoInput.value;
     if (todoText == "") {
       alert("Please write something for the todo");
     } else {
-      addTodoToLocalStorage(todoText);
-      appendTodoInHTML(todoText);
+      addTodoToLocalStorage({
+        text: todoText,
+        isCompleted: false,
+      });
+      appendTodoInHTML({
+        text: todoText,
+        isCompleted: false,
+      });
       todoInput.value = "";
     }
   });
@@ -67,9 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const todos = LoadTodos();
   todos.todoList.forEach((todo) => {
-    const newTodoItem = document.createElement("li");
-    newTodoItem.textContent = todo;
-    todoList.appendChild(newTodoItem);
+    appendTodoInHTML(todo);
   });
 });
 
